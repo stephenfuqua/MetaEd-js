1bf938167b0ba03b7e8a8d7406e580e75e758d06
David Doyle <david.doyle@doublelinepartners.com>
2017-06-24 19:13:36 -0500

    Domain Entity Extension Exists Only In Extension Namespace (#10)

---
+++ src/plugin/unified/validator/DomainEntityExtension/DomainEntityExtensionExistsOnlyInExtensionNamespace.js
@@ -0,0 +1,23 @@
+// @flow
+import type { Repository } from '../../../../core/model/Repository';
+import type { ValidationFailure } from '../../../../core/validator/ValidationFailure';
+import type { PropertyIndex } from '../../../../core/model/property/PropertyIndex';
+
+// eslint-disable-next-line no-unused-vars
+export function validate(repository: Repository, propertyIndex?: PropertyIndex): Array<ValidationFailure> {
+  const failures: Array<ValidationFailure> = [];
+  repository.entity.domainEntityExtension.forEach(entity => {
+    if (!entity.namespaceInfo.isExtension) {
+      failures.push({
+        validatorName: 'DomainEntityExtensionExistsOnlyInExtensionNamespace',
+        category: 'error',
+        message: `DomainEntity additions '${entity.metaEdName}' is not valid in core namespace '${entity.namespaceInfo.namespace}`,
+        sourceMap: entity.sourceMap.type,
+        fileMap: null,
+      });
+    }
+  });
+
+  return failures;
+}
+
