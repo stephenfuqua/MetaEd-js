68d4490f85bb3258a79e9c37d1c8010ef6ebb63a
Travis Meador <t.meador@wearedoubleline.com>
2017-10-05 14:19:06 -0500

    Add ModifyTotalInstructionalDaysToUseIntDiminisher (#51)

---
+++ packages/metaed-plugin-edfi-xsd/src/diminisher/ModifyTotalInstructionalDaysToUseIntDiminisher.js
@@ -0,0 +1,43 @@
+// @flow
+import R from 'ramda';
+import type {
+  EnhancerResult,
+  MetaEdEnvironment,
+  ModelBase,
+  ModelType,
+} from '../../../../packages/metaed-core/index';
+import type { ComplexType } from '../../src/model/schema/ComplexType';
+import type { Element } from '../../src/model/schema/Element';
+import { asElement } from '../../src/model/schema/Element';
+import { getEntity } from '../../../../packages/metaed-core/index';
+
+// Workaround for METAED-455: Force Data Type to xs:int in Xsd for TotalInstructionalDays
+// This problem is resolved for the 2.1 Data Standard through ticket DATASTD-869
+const enhancerName: string = 'ModifyTotalInstructionalDaysToUseIntDiminisher';
+const targetVersions: string = '2.0.0';
+
+const domainEntityNames: Array<string> = ['AcademicWeek', 'Session'];
+const domainEntityType: ModelType = 'domainEntity';
+const elementNameType: string = 'TotalInstructionalDays';
+const intType: string = 'xs:int';
+
+export function enhance(metaEd: MetaEdEnvironment): EnhancerResult {
+  if (metaEd.dataStandardVersion !== targetVersions) return { enhancerName, success: true };
+
+  domainEntityNames.forEach(domainEntityName => {
+    const entity: ?ModelBase = getEntity(metaEd.entity, domainEntityName, domainEntityType);
+    const complexType: ?ComplexType = entity != null
+      ? R.head(entity.data.edfiXsd.xsd_ComplexTypes)
+      : null;
+    const element: ?Element = complexType != null
+      ? complexType.items.map(x => asElement(x)).find(x => x.name === elementNameType && x.type === elementNameType)
+      : null;
+
+    if (element != null) element.type = intType;
+  });
+
+  return {
+    enhancerName,
+    success: true,
+  };
+}
---
+++ packages/metaed-plugin-edfi-xsd/test/diminisher/ModifyTotalInstructionalDaysToUseIntDiminisher.test.js
@@ -0,0 +1,138 @@
+// @flow
+import R from 'ramda';
+import type {
+  DomainEntity,
+  IntegerType,
+  MetaEdEnvironment,
+} from '../../../../packages/metaed-core/index';
+import {
+  addEntity,
+  newDomainEntity,
+  newIntegerType,
+  newMetaEdEnvironment,
+} from '../../../../packages/metaed-core/index';
+import { newComplexType } from '../../src/model/schema/ComplexType';
+import { newElement } from '../../src/model/schema/Element';
+import { newIntegerSimpleType } from '../../src/model/schema/IntegerSimpleType';
+import { enhance } from '../../src/diminisher/ModifyTotalInstructionalDaysToUseIntDiminisher';
+
+describe('when ModifyTotalInstructionalDaysToUseIntDiminisher diminishes academic week domain entity', () => {
+  const intType: string = 'xs:int';
+  let domainEntity: DomainEntity;
+
+  beforeAll(() => {
+    const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+    const domainEntityName: string = 'AcademicWeek';
+    const elementNameType: string = 'TotalInstructionalDays';
+
+    domainEntity = Object.assign(newDomainEntity(), {
+      metaEdName: domainEntityName,
+      data: {
+        edfiXsd: {
+          xsd_ComplexTypes: [
+            Object.assign(newComplexType(), {
+              name: domainEntityName,
+              items: [
+                Object.assign(newElement(), {
+                  name: elementNameType,
+                  type: elementNameType,
+                }),
+              ],
+            }),
+          ],
+        },
+      },
+    });
+    addEntity(metaEd.entity, domainEntity);
+
+    metaEd.dataStandardVersion = '2.0.0';
+    enhance(metaEd);
+  });
+
+  it('should have type set to int type', () => {
+    expect(R.head(R.head(domainEntity.data.edfiXsd.xsd_ComplexTypes).items).type).toBe(intType);
+  });
+});
+
+describe('when ModifyTotalInstructionalDaysToUseIntDiminisher diminishes session domain entity', () => {
+  const intType: string = 'xs:int';
+  let domainEntity: DomainEntity;
+
+  beforeAll(() => {
+    const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+    const domainEntityName: string = 'Session';
+    const elementNameType: string = 'TotalInstructionalDays';
+
+    domainEntity = Object.assign(newDomainEntity(), {
+      metaEdName: domainEntityName,
+      data: {
+        edfiXsd: {
+          xsd_ComplexTypes: [
+            Object.assign(newComplexType(), {
+              name: domainEntityName,
+              items: [
+                Object.assign(newElement(), {
+                  name: elementNameType,
+                  type: elementNameType,
+                }),
+              ],
+            }),
+          ],
+        },
+      },
+    });
+    addEntity(metaEd.entity, domainEntity);
+
+    metaEd.dataStandardVersion = '2.0.0';
+    enhance(metaEd);
+  });
+
+  it('should have type set to int type', () => {
+    expect(R.head(R.head(domainEntity.data.edfiXsd.xsd_ComplexTypes).items).type).toBe(intType);
+  });
+});
+
+describe('when ModifyTotalInstructionalDaysToUseIntDiminisher diminishes with no academic week or session', () => {
+  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+
+  beforeAll(() => {
+    const domainEntityName: string = 'DomainEntityName';
+    const integerTypeName: string = 'IntegerTypeName';
+
+    const domainEntity: DomainEntity = Object.assign(newDomainEntity(), {
+      metaEdName: domainEntityName,
+      data: {
+        edfiXsd: {
+          xsd_ComplexTypes: [
+            Object.assign(newComplexType(), {
+              name: domainEntityName,
+              items: [
+                Object.assign(newElement(), {
+                  name: 'ElementName',
+                  type: 'ElementType',
+                }),
+              ],
+            }),
+          ],
+        },
+      },
+    });
+    addEntity(metaEd.entity, domainEntity);
+
+    const integerType: IntegerType = Object.assign(newIntegerType(), {
+      metaEdName: integerTypeName,
+      data: {
+        edfiXsd: {
+          xsd_SimpleType: Object.assign(newIntegerSimpleType(), { name: integerTypeName, minValue: '1' }),
+        },
+      },
+    });
+    addEntity(metaEd.entity, integerType);
+
+    metaEd.dataStandardVersion = '2.0.0';
+  });
+
+  it('should run without error', () => {
+    expect(enhance(metaEd).success).toBe(true);
+  });
+});
