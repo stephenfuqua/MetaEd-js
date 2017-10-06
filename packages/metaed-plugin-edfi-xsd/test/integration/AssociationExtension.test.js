6cab98631dbb104c95bfa815be06c941b22f0f89
Brad Banister <brad@doublelinepartners.com>
2017-10-06 10:19:52 -0500

    association extension integration test

--- packages/metaed-plugin-edfi-xsd/test/integration/AssociationExtension.test.js
+++ packages/metaed-plugin-edfi-xsd/test/integration/AssociationExtension.test.js
@@ -1,16 +1,22 @@
 // @flow
-import xmlParser from 'xml-js';
+import { DOMParser } from 'xmldom';
+import xpath from 'xpath';
 import type { MetaEdEnvironment } from '../../../metaed-core/index';
 import { newMetaEdEnvironment, MetaEdTextBuilder, DomainEntityBuilder, AssociationBuilder, AssociationExtensionBuilder, NamespaceInfoBuilder } from '../../../metaed-core/index';
 import initializeUnifiedPlugin from './GetUnifiedPlugin';
 import initializeXsdPlugin from '../../src/edfiXsd';
 import { generate } from '../../src/generator/XsdGenerator';
 
+const xpathSelect = xpath.useNamespaces({
+  xs: 'http://www.w3.org/2001/XMLSchema',
+  ann: 'http://ed-fi.org/annotation',
+});
+
 describe('when generating xsd for association extension in extension namespace based on core association', () => {
   const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
 
-  const namespace: string = 'namespace';
-  const projectExtension: string = 'ProjectExtension';
+  const namespace: string = 'extension';
+  const projectExtension: string = 'EXTENSION';
   const coreEntity1: string = 'CoreEntity1';
   const coreEntity1Pk: string = 'CoreEntity1Pk';
   const coreEntity2: string = 'CoreEntity2';
@@ -62,23 +68,39 @@ describe('when generating xsd for association extension in extension namespace b
     initializeXsdPlugin().enhancer.forEach(enhance => enhance(metaEd));
 
     const generatorResult = generate(metaEd).generatedOutput;
-    coreResult = xmlParser.xml2js(generatorResult[0].resultString);
-    extensionResult = xmlParser.xml2js(generatorResult[1].resultString);
+    const coreResultString = generatorResult[0].resultString;
+    const extensionResultString = generatorResult[1].resultString;
+    coreResult = new DOMParser().parseFromString(coreResultString);
+    extensionResult = new DOMParser().parseFromString(extensionResultString);
   });
 
-  it('should do something', () => {
-    const xsSchema = coreResult.elements[1];
-
-    const schemaDocumentationAnnotation = xsSchema.elements[1];
-    expect(schemaDocumentationAnnotation.name).toBe('xs:annotation');
-    expect(schemaDocumentationAnnotation.elements[0].name).toBe('xs:documentation');
+  it('should generate core domain entity1', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreEntity1']", coreResult);
+    expect(elements).toHaveLength(1);
   });
 
-  it('should do something in extension', () => {
-    const xsSchema = extensionResult.elements[1];
+  it('should generate core domain entity2', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreEntity2']", coreResult);
+    expect(elements).toHaveLength(1);
+  });
 
-    const schemaDocumentationAnnotation = xsSchema.elements[1];
-    expect(schemaDocumentationAnnotation.name).toBe('xs:annotation');
-    expect(schemaDocumentationAnnotation.elements[0].name).toBe('xs:documentation');
+  it('should generate core association', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreAssociation']", coreResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate extension association', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='EXTENSION-CoreAssociationExtension']", extensionResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate extension association as extending core association', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='EXTENSION-CoreAssociationExtension']/xs:complexContent/xs:extension[@base='CoreAssociation']", extensionResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate extension association new property', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='EXTENSION-CoreAssociationExtension']/xs:complexContent/xs:extension/xs:sequence/xs:element[@name='ExtensionProperty']", extensionResult);
+    expect(elements).toHaveLength(1);
   });
 });
