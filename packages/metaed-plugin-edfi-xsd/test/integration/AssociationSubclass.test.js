4de8edb9a70c80bca88549e107f99f7f4a8a5e31
Brad Banister <brad@doublelinepartners.com>
2017-10-06 13:41:25 -0500

    association subclass integration test

---
+++ packages/metaed-plugin-edfi-xsd/test/integration/AssociationSubclass.test.js
@@ -0,0 +1,92 @@
+// @flow
+import type { MetaEdEnvironment } from '../../../metaed-core/index';
+import { newMetaEdEnvironment, MetaEdTextBuilder, DomainEntityBuilder, AssociationBuilder, AssociationSubclassBuilder, NamespaceInfoBuilder } from '../../../metaed-core/index';
+import { xpathSelect, enhanceAndGenerate } from './IntegrationTestHelper';
+
+describe('when generating xsd for association extension in extension namespace based on core association', () => {
+  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+
+  const namespace: string = 'extension';
+  const projectExtension: string = 'EXTENSION';
+  const coreEntity1: string = 'CoreEntity1';
+  const coreEntity1Pk: string = 'CoreEntity1Pk';
+  const coreEntity2: string = 'CoreEntity2';
+  const coreEntity2Pk: string = 'CoreEntity2Pk';
+  const coreAssociation: string = 'CoreAssociation';
+  const extensionAssociation: string = 'ExtensionAssociation';
+  const extensionAssociationProperty: string = 'ExtensionAssociationProperty';
+
+  let coreResult;
+  let extensionResult;
+
+  beforeAll(() => {
+    const domainEntityBuilder = new DomainEntityBuilder(metaEd, []);
+    const associationBuilder = new AssociationBuilder(metaEd, []);
+    const associationSubclassBuilder = new AssociationSubclassBuilder(metaEd, []);
+    const namespaceInfoBuilder = new NamespaceInfoBuilder(metaEd, []);
+    MetaEdTextBuilder.build()
+    .withBeginNamespace('edfi')
+
+    .withStartDomainEntity(coreEntity1)
+    .withDocumentation('doc')
+    .withIntegerIdentity(coreEntity1Pk, 'doc')
+    .withEndDomainEntity()
+
+    .withStartDomainEntity(coreEntity2)
+    .withDocumentation('doc')
+    .withIntegerIdentity(coreEntity2Pk, 'doc')
+    .withEndDomainEntity()
+
+    .withStartAssociation(coreAssociation)
+    .withDocumentation('doc')
+    .withAssociationDomainEntityProperty(coreEntity1, 'doc')
+    .withAssociationDomainEntityProperty(coreEntity2, 'doc')
+    .withEndAssociation()
+
+    .withEndNamespace()
+    .withBeginNamespace(namespace, projectExtension)
+
+    .withStartAssociationSubclass(extensionAssociation, coreAssociation)
+    .withDocumentation('doc')
+    .withIntegerProperty(extensionAssociationProperty, 'doc', true, false)
+    .withEndAssociationSubclass()
+
+    .withEndNamespace()
+    .sendToListener(domainEntityBuilder)
+    .sendToListener(associationBuilder)
+    .sendToListener(associationSubclassBuilder)
+    .sendToListener(namespaceInfoBuilder);
+
+    ({ coreResult, extensionResult } = enhanceAndGenerate(metaEd));
+  });
+
+  it('should generate core domain entity1', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreEntity1']", coreResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate core domain entity2', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreEntity2']", coreResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate core association', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreAssociation']", coreResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate extension association', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='EXTENSION-ExtensionAssociation']", extensionResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate extension association as extending core association', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='EXTENSION-ExtensionAssociation']/xs:complexContent/xs:extension[@base='CoreAssociation']", extensionResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate extension association new property', () => {
+    const elements = xpathSelect("/xs:schema/xs:complexType[@name='EXTENSION-ExtensionAssociation']/xs:complexContent/xs:extension/xs:sequence/xs:element[@name='ExtensionAssociationProperty']", extensionResult);
+    expect(elements).toHaveLength(1);
+  });
+});
