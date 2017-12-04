04bc1d9af99dea0cc807ffa959b14e749b11f468
Brad Banister <brad@doublelinepartners.com>
2017-12-04 15:27:53 -0600

    [METAED-701] warning that required properties on extension DEs and Assocs don't work with ODS/API

--- packages/metaed-plugin-edfi-odsapi/src/index.js
+++ packages/metaed-plugin-edfi-odsapi/src/index.js
@@ -3,10 +3,16 @@ import type { Validator, MetaEdPlugin } from 'metaed-core';
 import { newMetaEdPlugin } from 'metaed-core';
 
 import { validate as extendingSubclassOfEducationOrganizationProhibited } from './validator/UpcomingImprovements/ExtendingSubclassOfEducationOrganizationProhibited';
+import { validate as extendingStudentProgramAssociationOrSubclassProhibited } from './validator/UpcomingImprovements/ExtendingStudentProgramAssociationOrSubclassProhibited';
+import { validate as includingRequiredPropertiesInExtendedAssociationsProhibited } from './validator/UpcomingImprovements/IncludingRequiredPropertiesInExtendedAssociationsProhibited';
+import { validate as includingRequiredPropertiesInExtendedDomainEntitiesProhibited } from './validator/UpcomingImprovements/IncludingRequiredPropertiesInExtendedDomainEntitiesProhibited';
 
 function validatorList(): Array<Validator> {
   return [
+    extendingStudentProgramAssociationOrSubclassProhibited,
     extendingSubclassOfEducationOrganizationProhibited,
+    includingRequiredPropertiesInExtendedAssociationsProhibited,
+    includingRequiredPropertiesInExtendedDomainEntitiesProhibited,
   ];
 }
 
---
+++ packages/metaed-plugin-edfi-odsapi/test/validator/IncludingRequiredPropertiesInExtendedDomainEntitiesProhibited.test.js
@@ -0,0 +1,67 @@
+// @flow
+import { newMetaEdEnvironment, MetaEdTextBuilder, DomainEntityBuilder, DomainEntityExtensionBuilder } from 'metaed-core';
+import type { MetaEdEnvironment, ValidationFailure } from 'metaed-core';
+import { validate } from '../../src/validator/UpcomingImprovements/IncludingRequiredPropertiesInExtendedDomainEntitiesProhibited';
+
+describe('when a domain entity extension extends a domain entity with no required properties', () => {
+  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+  const entityName: string = 'EntityName';
+  let failures: Array<ValidationFailure>;
+
+  beforeAll(() => {
+    MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
+      .withStartDomainEntity(entityName)
+      .withDocumentation('doc')
+      .withBooleanProperty('PropertyName1', 'doc', true, false)
+      .withEndDomainEntity()
+
+      .withStartDomainEntityExtension(entityName)
+      .withBooleanProperty('OptionalProperty1', 'doc', false, false)
+      .withBooleanProperty('OptionalProperty2', 'doc', false, false)
+      .withEndDomainEntitySubclass()
+      .withEndNamespace()
+
+      .sendToListener(new DomainEntityBuilder(metaEd, []))
+      .sendToListener(new DomainEntityExtensionBuilder(metaEd, []));
+
+    failures = validate(metaEd);
+  });
+
+  it('should have no validation failures', () => {
+    expect(failures).toHaveLength(0);
+  });
+});
+
+describe('when a domain entity extension extends a domain entity with a required property', () => {
+  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+  const entityName: string = 'EntityName';
+  let failures: Array<ValidationFailure>;
+
+  beforeAll(() => {
+    MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
+      .withStartDomainEntity(entityName)
+      .withDocumentation('doc')
+      .withBooleanProperty('PropertyName1', 'doc', true, false)
+      .withEndDomainEntity()
+
+      .withStartDomainEntityExtension(entityName)
+      .withBooleanProperty('RequiredProperty', 'doc', true, false)
+      .withEndDomainEntitySubclass()
+      .withEndNamespace()
+
+      .sendToListener(new DomainEntityBuilder(metaEd, []))
+      .sendToListener(new DomainEntityExtensionBuilder(metaEd, []));
+
+    failures = validate(metaEd);
+  });
+
+  it('should have validation failures', () => {
+    expect(failures).toHaveLength(1);
+    expect(failures[0].validatorName).toBe('IncludingRequiredPropertiesInExtendedDomainEntitiesProhibited');
+    expect(failures[0].category).toBe('warning');
+    expect(failures[0].message).toMatchSnapshot('when a domain entity extension extends a domain entity with a required property should have validation failure -> message');
+    expect(failures[0].sourceMap).toMatchSnapshot('when a domain entity extension extends a domain entity with a required property should have validation failure -> sourceMap');
+  });
+});
