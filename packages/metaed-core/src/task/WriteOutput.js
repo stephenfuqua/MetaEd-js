b4d18186ef54dbf2d04b3e1789cda10e37f3b0d3
raf90-edfi <31217788+raf90-edfi@users.noreply.github.com>
2017-12-11 13:17:11 -0600

    Add support for ResultStream in the GeneratorResults to be written if… (#65)
    * Add support for ResultStream in the GeneratorResults to be written if ResultString is empty.
    
    * added support for promises and added output message in case of empty results
    
    * fix bug
    
    * v1.1.1-dev.19

--- packages/metaed-core/src/task/WriteOutput.js
+++ packages/metaed-core/src/task/WriteOutput.js
@@ -3,8 +3,18 @@ import ffs from 'final-fs';
 import path from 'path';
 import winston from 'winston';
 import type { State } from '../State';
+import type { GeneratorResult } from '../generator/GeneratorResult';
 
 winston.cli();
+
+function writeOutputFiles(result: GeneratorResult, outputDirectory: string) {
+  result.generatedOutput.forEach(output => {
+    if (!ffs.existsSync(`${outputDirectory}/${output.folderName}`)) ffs.mkdirRecursiveSync(`${outputDirectory}/${output.folderName}`);
+    if (output.resultString) ffs.writeFileSync(`${outputDirectory}/${output.folderName}/${output.fileName}`, output.resultString, 'utf-8');
+    else if (output.resultStream) ffs.writeFileSync(`${outputDirectory}/${output.folderName}/${output.fileName}`, output.resultStream);
+    else winston.info(`No output stream or string for ${result.generatorName}`);
+  });
+}
 
 export function execute(state: State): void {
   let outputDirectory: string = '';
@@ -24,10 +34,14 @@ export function execute(state: State): void {
   ffs.mkdirRecursiveSync(outputDirectory);
   if (ffs.existsSync(outputDirectory)) {
     state.generatorResults.forEach(result => {
-      result.generatedOutput.forEach(output => {
-        if (!ffs.existsSync(`${outputDirectory}/${output.folderName}`)) ffs.mkdirRecursiveSync(`${outputDirectory}/${output.folderName}`);
-        ffs.writeFileSync(`${outputDirectory}/${output.folderName}/${output.fileName}`, output.resultString, 'utf-8');
-      });
+      // if (result is a Promise)
+      if (result.then) {
+        winston.info('Resolving Promise:');
+        // $FlowIgnore - flow was expecting a GeneratorResults not a promise
+        result.then((resolvedResult) => {
+          writeOutputFiles(resolvedResult, outputDirectory);
+        });
+      } else writeOutputFiles(result, outputDirectory);
     });
   }
 }
