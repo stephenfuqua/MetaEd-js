ad5e394055358590c3c93943045ec1843f4a771c
Brad Banister <brad@doublelinepartners.com>
2018-04-03 07:59:13 -0500

    [METAED-759] also prevent descriptor properties from having Descriptor suffix

---
+++ packages/metaed-plugin-edfi-unified/test/validator/DescriptorProperty/DescriptorPropertyNameCannotEndInDescriptor.test.js
@@ -0,0 +1,65 @@
+// @flow
+import { newMetaEdEnvironment, MetaEdTextBuilder, DomainEntityBuilder, DescriptorBuilder } from 'metaed-core';
+import type { MetaEdEnvironment, ValidationFailure } from 'metaed-core';
+import { validate } from '../../../src/validator/DescriptorProperty/DescriptorPropertyNameCannotEndInDescriptor';
+
+describe('when descriptor property has valid name', () => {
+  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+  let failures: Array<ValidationFailure>;
+
+  beforeAll(() => {
+    MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
+      .withStartDomainEntity('EntityName')
+      .withDocumentation('doc')
+      .withStringIdentity('PropertyName', 'doc', '100')
+      .withDescriptorProperty('ValidName', 'doc', true, false)
+      .withEndDomainEntity()
+      .withEndNamespace()
+
+      .sendToListener(new DescriptorBuilder(metaEd, []))
+      .sendToListener(new DomainEntityBuilder(metaEd, []));
+
+    failures = validate(metaEd);
+  });
+
+  it('should build one domain entity', () => {
+    expect(metaEd.entity.domainEntity.size).toBe(1);
+  });
+
+  it('should have no validation failures', () => {
+    expect(failures).toHaveLength(0);
+  });
+});
+
+describe('when descriptor property has invalid name', () => {
+  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+  let failures: Array<ValidationFailure>;
+
+  beforeAll(() => {
+    MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
+      .withStartDomainEntity('EntityName')
+      .withDocumentation('doc')
+      .withStringIdentity('PropertyName', 'doc', '100')
+      .withDescriptorProperty('InvalidNameDescriptor', 'doc', true, false)
+      .withEndDomainEntity()
+      .withEndNamespace()
+
+      .sendToListener(new DomainEntityBuilder(metaEd, []));
+
+    failures = validate(metaEd);
+  });
+
+  it('should build one domain entity', () => {
+    expect(metaEd.entity.domainEntity.size).toBe(1);
+  });
+
+  it('should have validation failures', () => {
+    expect(failures).toHaveLength(1);
+    expect(failures[0].validatorName).toBe('DescriptorPropertyNameCannotEndInDescriptor');
+    expect(failures[0].category).toBe('error');
+    expect(failures[0].message).toMatchSnapshot();
+    expect(failures[0].sourceMap).toMatchSnapshot();
+  });
+});
