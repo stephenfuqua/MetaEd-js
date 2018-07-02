4faa03fce2ac7fd7c531aba92b8411a2e6632313
Brad Banister <brad@doublelinepartners.com>
2018-07-02 14:15:14 -0500

    add 3.1 and 2.4 to IDE API version selector

--- packages/atom-metaed/lib/ManageConfiguration.js
+++ packages/atom-metaed/lib/ManageConfiguration.js
@@ -22,7 +22,9 @@ const odsApiVersionSupport: Map<string, Array<string>> = new Map([
   ['2.2', ['2.0.1']],
   ['2.3', ['2.0.1']],
   ['2.3.1', ['2.0.1']],
+  ['2.4', ['2.0.1']],
   ['3.0', ['3.0']],
+  ['3.1', ['3.0']],
 ]);
 
 export function switchCoreDsProjectOnDsChange(disposableTracker: CompositeDisposable) {
