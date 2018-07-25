c6c396eb9479db1d0b9c27d14f8dae152e6939f1
Brad Banister <brad@doublelinepartners.com>
2018-07-25 10:25:47 -0500

    [METAED-860] change event functionality pushed out of 2.4

--- packages/metaed-plugin-edfi-ods-changeevent/src/enhancer/ChangeEventIndicator.js
+++ packages/metaed-plugin-edfi-ods-changeevent/src/enhancer/ChangeEventIndicator.js
@@ -6,7 +6,7 @@ export function changeEventPossible(metaEd: MetaEdEnvironment): boolean {
   const edfiOdsPlugin: ?PluginEnvironment = metaEd.plugin.get('edfiOds');
   if (edfiOdsPlugin == null) return false;
 
-  if (versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.4.0 <3.0.0')) return true;
+  if (versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.5.0 <3.0.0')) return true;
   return versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=3.1.0');
 }
 
@@ -14,7 +14,7 @@ export function changeEventIndicated(metaEd: MetaEdEnvironment, currentNamespace
   const edfiOdsPlugin: ?PluginEnvironment = metaEd.plugin.get('edfiOds');
   if (edfiOdsPlugin == null) return false;
 
-  if (versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.4.0 <3.0.0')) return !currentNamespace.isExtension;
+  if (versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.5.0 <3.0.0')) return !currentNamespace.isExtension;
   return versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=3.1.0');
 }
 
@@ -22,7 +22,7 @@ export function twoDotXIndicated(metaEd: MetaEdEnvironment, currentNamespace: Na
   const edfiOdsPlugin: ?PluginEnvironment = metaEd.plugin.get('edfiOds');
   if (edfiOdsPlugin == null) return false;
 
-  if (!versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.4.0 <3.0.0')) return false;
+  if (!versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.5.0 <3.0.0')) return false;
   return !currentNamespace.isExtension;
 }
 
--- packages/metaed-plugin-edfi-ods/src/enhancer/ChangeEventIndicator.js
+++ packages/metaed-plugin-edfi-ods/src/enhancer/ChangeEventIndicator.js
@@ -6,6 +6,6 @@ export function changeEventIndicated(metaEd: MetaEdEnvironment, currentNamespace
   const edfiOdsPlugin: ?PluginEnvironment = metaEd.plugin.get('edfiOds');
   if (edfiOdsPlugin == null) return false;
 
-  if (versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.4.0 <3.0.0')) return !currentNamespace.isExtension;
+  if (versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=2.5.0 <3.0.0')) return !currentNamespace.isExtension;
   return versionSatisfies(edfiOdsPlugin.targetTechnologyVersion, '>=3.1.0');
 }
