6bf00c4990339f38c4e26970b7909d27c1342cff
Brad Banister <brad@doublelinepartners.com>
2018-12-07 17:21:48 -0600

    checkpoint

--- packages/generator-metaed-plugin/generators/app/templates/src/generator/DomainEntityNameGenerator.js
+++ packages/generator-metaed-plugin/generators/app/templates/src/generator/DomainEntityNameGenerator.js
@@ -1,4 +1,5 @@
 // @flow
+import { promisify } from 'util';
 import path from 'path';
 import fs from 'fs';
 import handlebars from 'handlebars';
@@ -6,18 +7,11 @@ import handlebars from 'handlebars';
 import type { MetaEdEnvironment, GeneratorResult, GeneratedOutput, ModelBase } from 'metaed-core';
 import { getAllEntitiesOfType } from 'metaed-core';
 
+// node 8.0+ wrapper to enable async/await on file read
+const readFile = promisify(fs.readFile);
+
 // Handlebars instance scoped for this plugin
 export const domainEntityNameHandlebars = handlebars.create();
-
-// Read the Handlebars template file
-function templateString(templateName: string) {
-  return fs.readFileSync(path.join(__dirname, 'templates', `${templateName}.hbs`)).toString();
-}
-
-// Compile the Handlebars template file
-export function templateNamed(templateName: string) {
-  return domainEntityNameHandlebars.compile(templateString(templateName));
-}
 
 // standard generator function signature
 export async function generate(metaEd: MetaEdEnvironment): Promise<GeneratorResult> {
@@ -26,8 +20,12 @@ export async function generate(metaEd: MetaEdEnvironment): Promise<GeneratorResu
   // Get all Domain Entities in all namespaces, as ModelBase type so there is no need to cast
   const domainEntities: Array<ModelBase> = getAllEntitiesOfType(metaEd, 'domainEntity');
 
+  // Compile the Handlebars template file, return custom handlebars function to apply data to our template
+  const templateBuffer = await readFile(path.join(__dirname, 'templates', 'DomainEntityNames.hbs'));
+  const templateFunction: Object => string = domainEntityNameHandlebars.compile(templateBuffer.toString());
+
   // Feed Domain Entities to DomainEntityNames.hbs template
-  const resultString: string = templateNamed('DomainEntityNames')({ domainEntities });
+  const resultString: string = templateFunction({ domainEntities });
 
   // Provide result with file metadata
   // Often MetaEd generates one file per namespace, but here we are putting information from all namespaces in one file
