65b27ddb57967ca777ff993a73e4708fd44fbf7d
Brad Banister <brad@doublelinepartners.com>
2020-01-28 14:09:06 -0600

    [METAED-1108] hardcoded build working using vscode terminal

--- packages/vscode-metaed/src/extension.ts
+++ packages/vscode-metaed/src/extension.ts
@@ -1,24 +1,66 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-
 // eslint-disable-next-line import/no-unresolved
 import * as vscode from 'vscode';
+import path from 'path';
+import fs from 'fs-extra';
+import tmp from 'tmp-promise';
+import { MetaEdConfiguration } from 'metaed-core';
 
-// this method is called when your extension is activated
+function createMetaEdConfiguration(): MetaEdConfiguration | undefined {
+  const { workspaceFolders } = vscode.workspace;
+  if (workspaceFolders == null) return undefined;
+  const workspaceRootPath = workspaceFolders[0].uri.fsPath;
+
+  return {
+    artifactDirectory: path.join(workspaceRootPath, 'MetaEdOutput'),
+    deployDirectory: '',
+    pluginTechVersion: {},
+    projects: [
+      {
+        namespaceName: 'EdFi',
+        projectName: 'Ed-Fi',
+        projectVersion: '3.2.0',
+        projectExtension: '',
+      },
+    ],
+    projectPaths: [workspaceRootPath],
+    pluginConfigDirectories: [],
+    defaultPluginTechVersion: '3.2.0',
+    allianceMode: false,
+  };
+}
+
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  // console.log('Congratulations, your extension "vscode-metaed" is now active!');
+  // eslint-disable-next-line no-console
+  console.log('Congratulations, your extension "vscode-metaed" is now active!');
+
+  let NEXT_TERM_ID = 1;
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+  const disposable = vscode.commands.registerCommand('extension.metaed.build', async () => {
     // The code you place here will be executed every time your command is executed
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
+    vscode.window.showInformationMessage('Building MetaEd...');
+    const metaEdConsoleDirectory = path.resolve(__dirname, '../..', 'metaed-console');
+
+    const terminal: vscode.Terminal = vscode.window.createTerminal(
+      `Ext Terminal #${NEXT_TERM_ID}`,
+      'C:\\Windows\\System32\\cmd.exe',
+    );
+    NEXT_TERM_ID += 1;
+
+    terminal.show(true);
+
+    tmp.setGracefulCleanup();
+    const tempConfigurationPath = await tmp.tmpName({ prefix: 'MetaEdConfig-VsCode-', postfix: '.json' });
+    const metaEdConfiguration = createMetaEdConfiguration();
+    await fs.outputJson(tempConfigurationPath, { metaEdConfiguration }, { spaces: 2 });
+
+    terminal.sendText(`node ${metaEdConsoleDirectory} --config ${tempConfigurationPath}`, true);
+
+    // TODO: remove tempfile after awaiting for build to finish
   });
 
   context.subscriptions.push(disposable);
