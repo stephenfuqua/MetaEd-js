2dafa85df20195b4a4926b0e996b5de50f1f04b4
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2021-10-08 14:32:00 -0500

    [METAED-1231] Allow common subclass to rely on parent identity (#23)
    * Run unit tests directly in this package
    
    * Refactor and add test that fails
    
    * This does not work, sharing for discussion only
    
    * Remove local test configuration
    
    * Add common subclass builder
    Introduce some constants
    Add another test
    
    * Need the commonSubclass enhancer to complete the model initialization
    
    * Use native JS type
    Improve identity property lookup for superclass

--- packages/metaed-plugin-edfi-unified-advanced/src/validator/CommonProperty/CommonPropertyCollectionTargetMustContainIdentity.ts
+++ packages/metaed-plugin-edfi-unified-advanced/src/validator/CommonProperty/CommonPropertyCollectionTargetMustContainIdentity.ts
@@ -1,11 +1,20 @@
-import { MetaEdEnvironment, ValidationFailure } from 'metaed-core';
+import { MetaEdEnvironment, ValidationFailure, CommonProperty } from 'metaed-core';
+
+function hasIdentity(commonProperty: CommonProperty): boolean {
+  return (
+    commonProperty.referencedEntity.identityProperties.length > 0 ||
+    (commonProperty.referencedEntity.baseEntity != null &&
+      commonProperty.referencedEntity.baseEntity.identityProperties.length > 0)
+  );
+}
 
 export function validate(metaEd: MetaEdEnvironment): ValidationFailure[] {
   const failures: ValidationFailure[] = [];
 
   metaEd.propertyIndex.common.forEach(commonProperty => {
     if (!commonProperty.isOptionalCollection && !commonProperty.isRequiredCollection) return;
-    if (!commonProperty.referencedEntity.properties.some(property => property.isPartOfIdentity)) {
+
+    if (!hasIdentity(commonProperty)) {
       failures.push({
         validatorName: 'CommonPropertyCollectionTargetMustContainIdentity',
         category: 'error',
--- packages/metaed-plugin-edfi-unified-advanced/test/validator/CommonProperty/CommonPropertyCollectionTargetMustContainIdentity.test.ts
+++ packages/metaed-plugin-edfi-unified-advanced/test/validator/CommonProperty/CommonPropertyCollectionTargetMustContainIdentity.test.ts
@@ -6,101 +6,225 @@ import {
   NamespaceBuilder,
   MetaEdEnvironment,
   ValidationFailure,
+  CommonSubclassBuilder,
 } from 'metaed-core';
-import { commonReferenceEnhancer } from 'metaed-plugin-edfi-unified';
+import { commonReferenceEnhancer, commonSubclassBaseClassEnhancer } from 'metaed-plugin-edfi-unified';
 import { validate } from '../../../src/validator/CommonProperty/CommonPropertyCollectionTargetMustContainIdentity';
 
-describe('when validating collection common property target has no identity', (): void => {
-  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
-  let failures: ValidationFailure[];
-  let coreNamespace: any = null;
+describe('when validating collection common property target', (): void => {
+  const COMMON_ENTITY_NAME = 'EntityName';
+  const DOMAIN_ENTITY_NAME = 'EntityName2';
+  const COMMON_SUB_ENTITY_NAME = 'CommonSubclass';
 
-  beforeAll(() => {
-    MetaEdTextBuilder.build()
-      .withBeginNamespace('EdFi')
-      .withStartCommon('EntityName')
-      .withDocumentation('EntityDocumentation')
-      .withStringProperty('PropertyName1', 'PropertyDocumentation', true, false, '100')
-      .withEndCommon()
+  describe('given the property does not have an identity', (): void => {
+    describe('and given the property has no superclass', (): void => {
+      const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+      let failures: ValidationFailure[];
+      let coreNamespace: any = null;
 
-      .withStartDomainEntity('EntityName2')
-      .withDocumentation('EntityDocumentation')
-      .withCommonProperty('EntityName', 'PropertyDocumentation', true, true)
-      .withEndDomainEntity()
-      .withEndNamespace()
+      beforeAll(() => {
+        MetaEdTextBuilder.build()
+          .withBeginNamespace('EdFi')
+          .withStartCommon(COMMON_ENTITY_NAME)
+          .withDocumentation('EntityDocumentation')
+          .withStringProperty('PropertyName1', 'PropertyDocumentation', true, false, '100')
+          .withEndCommon()
 
-      .sendToListener(new NamespaceBuilder(metaEd, []))
-      .sendToListener(new DomainEntityBuilder(metaEd, []))
-      .sendToListener(new CommonBuilder(metaEd, []));
+          .withStartDomainEntity(DOMAIN_ENTITY_NAME)
+          .withDocumentation('EntityDocumentation')
+          .withCommonProperty(COMMON_ENTITY_NAME, 'PropertyDocumentation', true, true)
+          .withEndDomainEntity()
+          .withEndNamespace()
 
-    coreNamespace = metaEd.namespace.get('EdFi');
-    commonReferenceEnhancer(metaEd);
+          .sendToListener(new NamespaceBuilder(metaEd, []))
+          .sendToListener(new DomainEntityBuilder(metaEd, []))
+          .sendToListener(new CommonBuilder(metaEd, []));
 
-    failures = validate(metaEd);
+        coreNamespace = metaEd.namespace.get('EdFi');
+        commonReferenceEnhancer(metaEd);
+
+        failures = validate(metaEd);
+      });
+
+      it('should build one common', (): void => {
+        expect(coreNamespace.entity.common.size).toBe(1);
+      });
+
+      it('should build one domain entity', (): void => {
+        expect(coreNamespace.entity.domainEntity.size).toBe(1);
+      });
+
+      it('should have validation failure for property', (): void => {
+        expect(failures[0].validatorName).toBe('CommonPropertyCollectionTargetMustContainIdentity');
+        expect(failures[0].category).toBe('error');
+        expect(failures[0].message).toMatchInlineSnapshot(
+          `"Common property EntityName cannot be used as a collection because Common EntityName does not have any identity properties."`,
+        );
+        expect(failures[0].sourceMap).toMatchInlineSnapshot(`
+          Object {
+            "column": 11,
+            "line": 13,
+            "tokenText": "EntityName",
+          }
+        `);
+      });
+    });
+
+    describe('and given the property has a superclass also without identity', (): void => {
+      const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+      let failures: ValidationFailure[];
+      let coreNamespace: any = null;
+
+      beforeAll(() => {
+        MetaEdTextBuilder.build()
+          .withBeginNamespace('EdFi')
+          .withStartCommon(COMMON_ENTITY_NAME)
+          .withDocumentation('EntityDocumentation')
+          // Does not have an identity
+          .withStringProperty('PropertyName1', 'PropertyDocumentation', true, false, '100')
+          .withEndCommon()
+
+          .withStartCommonSubclass(COMMON_SUB_ENTITY_NAME, COMMON_ENTITY_NAME)
+          // Does not have an identity
+          .withStringProperty('PropertyName2', 'Property2Documentation', true, false, '100')
+          .withEndCommonSubclass()
+
+          .withStartDomainEntity(DOMAIN_ENTITY_NAME)
+          .withDocumentation('EntityDocumentation')
+          .withCommonProperty(COMMON_SUB_ENTITY_NAME, 'PropertyDocumentation', true, true)
+          .withEndDomainEntity()
+          .withEndNamespace()
+
+          .sendToListener(new NamespaceBuilder(metaEd, []))
+          .sendToListener(new DomainEntityBuilder(metaEd, []))
+          .sendToListener(new CommonBuilder(metaEd, []))
+          .sendToListener(new CommonSubclassBuilder(metaEd, []));
+
+        coreNamespace = metaEd.namespace.get('EdFi');
+        commonReferenceEnhancer(metaEd);
+        commonSubclassBaseClassEnhancer(metaEd);
+
+        failures = validate(metaEd);
+      });
+
+      it('should build one commons', (): void => {
+        expect(coreNamespace.entity.common.size).toBe(1);
+      });
+
+      it('should build one domain entity', (): void => {
+        expect(coreNamespace.entity.domainEntity.size).toBe(1);
+      });
+
+      it('should have one validation failure', (): void => {
+        expect(failures).toHaveLength(1);
+      });
+
+      it('should have validation failure for property', (): void => {
+        expect(failures[0].validatorName).toBe('CommonPropertyCollectionTargetMustContainIdentity');
+        expect(failures[0].category).toBe('error');
+        expect(failures[0].message).toMatchInlineSnapshot(
+          `"Common property ${COMMON_SUB_ENTITY_NAME} cannot be used as a collection because Common Subclass ${COMMON_SUB_ENTITY_NAME} does not have any identity properties."`,
+        );
+        expect(failures[0].sourceMap).toMatchInlineSnapshot(`
+          Object {
+            "column": 11,
+            "line": 19,
+            "tokenText": "${COMMON_SUB_ENTITY_NAME}",
+          }
+        `);
+      });
+    });
+
+    describe('and given the property has a superclass with identity', (): void => {
+      const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+      let failures: ValidationFailure[];
+      let coreNamespace: any = null;
+
+      beforeAll(() => {
+        MetaEdTextBuilder.build()
+          .withBeginNamespace('EdFi')
+          .withStartCommon(COMMON_ENTITY_NAME)
+          .withDocumentation('EntityDocumentation')
+          // Has an identity
+          .withStringIdentity('PropertyName1', 'PropertyDocumentation', '100')
+          .withEndCommon()
+
+          .withStartCommonSubclass(COMMON_SUB_ENTITY_NAME, COMMON_ENTITY_NAME)
+          // Does not have an identity
+          .withStringProperty('PropertyName2', 'Property2Documentation', true, false, '100')
+          .withEndCommonSubclass()
+
+          .withStartDomainEntity(DOMAIN_ENTITY_NAME)
+          .withDocumentation('EntityDocumentation')
+          .withCommonProperty(COMMON_SUB_ENTITY_NAME, 'PropertyDocumentation', true, true)
+          .withEndDomainEntity()
+          .withEndNamespace()
+
+          .sendToListener(new NamespaceBuilder(metaEd, []))
+          .sendToListener(new DomainEntityBuilder(metaEd, []))
+          .sendToListener(new CommonBuilder(metaEd, []))
+          .sendToListener(new CommonSubclassBuilder(metaEd, []));
+
+        coreNamespace = metaEd.namespace.get('EdFi');
+        commonReferenceEnhancer(metaEd);
+        commonSubclassBaseClassEnhancer(metaEd);
+
+        failures = validate(metaEd);
+      });
+
+      it('should build one common', (): void => {
+        expect(coreNamespace.entity.common.size).toBe(1);
+      });
+
+      it('should build one domain entity', (): void => {
+        expect(coreNamespace.entity.domainEntity.size).toBe(1);
+      });
+
+      it('should have no validation failures', (): void => {
+        expect(failures).toHaveLength(0);
+      });
+    });
   });
 
-  it('should build one common', (): void => {
-    expect(coreNamespace.entity.common.size).toBe(1);
-  });
+  describe('given the property has an identity', (): void => {
+    const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+    let failures: ValidationFailure[];
+    let coreNamespace: any = null;
 
-  it('should build one domain entity', (): void => {
-    expect(coreNamespace.entity.domainEntity.size).toBe(1);
-  });
+    beforeAll(() => {
+      MetaEdTextBuilder.build()
+        .withBeginNamespace('EdFi')
+        .withStartCommon(COMMON_ENTITY_NAME)
+        .withDocumentation('EntityDocumentation')
+        .withStringIdentity('PropertyName1', 'PropertyDocumentation', '100')
+        .withEndCommon()
 
-  it('should have validation failure for property', (): void => {
-    expect(failures[0].validatorName).toBe('CommonPropertyCollectionTargetMustContainIdentity');
-    expect(failures[0].category).toBe('error');
-    expect(failures[0].message).toMatchInlineSnapshot(
-      `"Common property EntityName cannot be used as a collection because Common EntityName does not have any identity properties."`,
-    );
-    expect(failures[0].sourceMap).toMatchInlineSnapshot(`
-      Object {
-        "column": 11,
-        "line": 13,
-        "tokenText": "EntityName",
-      }
-    `);
-  });
-});
+        .withStartDomainEntity(DOMAIN_ENTITY_NAME)
+        .withDocumentation('EntityDocumentation')
+        .withCommonProperty(COMMON_ENTITY_NAME, 'PropertyDocumentation', true, true)
+        .withEndDomainEntity()
+        .withEndNamespace()
 
-describe('when validating collection common property target has an identity', (): void => {
-  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
-  let failures: ValidationFailure[];
-  let coreNamespace: any = null;
+        .sendToListener(new NamespaceBuilder(metaEd, []))
+        .sendToListener(new DomainEntityBuilder(metaEd, []))
+        .sendToListener(new CommonBuilder(metaEd, []));
 
-  beforeAll(() => {
-    MetaEdTextBuilder.build()
-      .withBeginNamespace('EdFi')
-      .withStartCommon('EntityName')
-      .withDocumentation('EntityDocumentation')
-      .withStringIdentity('PropertyName1', 'PropertyDocumentation', '100')
-      .withEndCommon()
+      coreNamespace = metaEd.namespace.get('EdFi');
+      commonReferenceEnhancer(metaEd);
 
-      .withStartDomainEntity('EntityName2')
-      .withDocumentation('EntityDocumentation')
-      .withCommonProperty('EntityName', 'PropertyDocumentation', true, true)
-      .withEndDomainEntity()
-      .withEndNamespace()
+      failures = validate(metaEd);
+    });
 
-      .sendToListener(new NamespaceBuilder(metaEd, []))
-      .sendToListener(new DomainEntityBuilder(metaEd, []))
-      .sendToListener(new CommonBuilder(metaEd, []));
+    it('should build one common', (): void => {
+      expect(coreNamespace.entity.common.size).toBe(1);
+    });
 
-    coreNamespace = metaEd.namespace.get('EdFi');
-    commonReferenceEnhancer(metaEd);
+    it('should build one domain entity', (): void => {
+      expect(coreNamespace.entity.domainEntity.size).toBe(1);
+    });
 
-    failures = validate(metaEd);
-  });
-
-  it('should build one common', (): void => {
-    expect(coreNamespace.entity.common.size).toBe(1);
-  });
-
-  it('should build one domain entity', (): void => {
-    expect(coreNamespace.entity.domainEntity.size).toBe(1);
-  });
-
-  it('should have no validation failures', (): void => {
-    expect(failures).toHaveLength(0);
+    it('should have no validation failures', (): void => {
+      expect(failures).toHaveLength(0);
+    });
   });
 });
