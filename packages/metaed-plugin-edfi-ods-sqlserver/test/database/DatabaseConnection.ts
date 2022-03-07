7063e239636f21617dcb5deadac095ab18b4a095
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2022-03-07 14:27:21 -0600

    [METAED-1253] Change from mssqlnodev8 to tedious (#41)
    * v3.0.1-dev.0
    
    * Change from mssqlnodev8 to tedious
    
    Co-authored-by: EdFiBuildAgent <devops@ed-fi.org>

--- packages/metaed-plugin-edfi-ods-sqlserver/test/database/DatabaseConnection.ts
+++ packages/metaed-plugin-edfi-ods-sqlserver/test/database/DatabaseConnection.ts
@@ -1,36 +1,37 @@
 import winston from 'winston';
-import { Connection, ConnectionPool, Request, Transaction } from 'mssql/msnodesqlv8';
 import { highlight } from 'cli-highlight';
+import sql from 'mssql';
 
 export const testDatabaseName = 'MetaEd_Ods_Integration_Tests';
 
 winston.configure({ transports: [new winston.transports.Console()], format: winston.format.cli() });
 winston.level = 'info';
-// if (process.env.TEAMCITY_VERSION != null) {
-//   winston.level = 'debug';
-// } else {
-//   winston.level = 'info';
-// }
-const highlightSql = (sql: string) => highlight(sql, { language: 'sql', ignoreIllegals: true });
+const highlightSql = (statement: string) => highlight(statement, { language: 'sql', ignoreIllegals: true });
 
 export interface Pool {
-  transaction?: Transaction;
-  connection?: ConnectionPool;
+  transaction?: sql.Transaction;
+  connection?: sql.ConnectionPool;
 }
+
 export const pools: Map<string, Pool> = new Map();
 export const retryCount = 5;
 
-export const newConfig = () => ({
-  database: '',
-  appName: 'MetaEd',
-  server: 'localhost',
+export const newConfig: sql.config = {
+  database: process.env.METAED_DATABASE || 'MetaEd_Ods_Integration_Tests',
+  server: process.env.MSSQL_SERVER || 'localhost',
+  port: process.env.MSSQL_PORT || 1433,
   connectionTimeout: 15000,
   requestTimeout: 15000,
+  driver: 'tedious',
+  // Note: TEDIOUS does not support integrated security, therefore *MUST* use username & password.
+  user: process.env.MSSQL_USERNAME || 'metaed',
+  password: process.env.MSSQL_PASSWORD || 'metaed-test',
   options: {
-    trustedConnection: true,
+    encrypt: process.env.MSSQL_ENCRYPT === 'true' || false,
+    trustServerCertificate: process.env.MSSQL_TRUST_CERTIFICATE === 'true' || false,
     abortTransactionOnError: true,
   },
-});
+};
 
 export async function disconnect(databaseName: string): Promise<void> {
   if (pools.has(databaseName)) {
@@ -61,25 +62,23 @@ export async function disconnectAll() {
   pools.clear();
 }
 
-export async function connect(databaseName: string, retry: number = retryCount): Promise<ConnectionPool> {
+export async function connect(databaseName: string, retry: number = retryCount): Promise<sql.ConnectionPool> {
   if (pools.has(databaseName)) {
     return pools.get(databaseName);
   }
 
   let pool: Pool = {};
   try {
-    pool.connection = await new ConnectionPool({ ...newConfig(), database: databaseName });
+    pool.connection = await new sql.ConnectionPool({ ...newConfig, database: databaseName });
     winston.verbose(`[${databaseName}] created connection pool`);
     if (!pool.connection.connected && !pool.connection.connecting) {
       await pool.connection.connect();
     }
 
-    pool.transaction = await new Transaction(pool.connection);
+    pool.transaction = await new sql.Transaction(pool.connection);
     await pool.transaction.begin();
     winston.verbose(`[${databaseName}] ---------- begin transaction ----------`);
   } catch (error) {
-    pool.connection.close();
-
     // NOTE: This is a work around for the following error. This seems to occur most often when a fresh database has been
     // created and a connection attempt is made while it is in transition.
     // Error: [Microsoft][SQL Server Native Client 11.0]TCP Provider: An existing connection was forcibly closed by the remote host.
@@ -128,17 +127,17 @@ export async function rollbackTransaction(databaseName: string): Promise<void> {
   }
 }
 
-export async function query(connection: Connection | Transaction, action: string, sql: string): Promise<any> {
+export async function query(connection: sql.Connection | sql.Transaction, action: string, statement: string): Promise<any> {
   const databaseName: string = connection.config ? connection.config.database : connection.parent.config.database;
   winston.verbose(`[${databaseName}] ${action}`);
-  winston.verbose('\n', highlightSql(sql));
-  const result = await new Request(connection).query(sql);
+  winston.verbose('\n', highlightSql(statement));
+  const result = await new sql.Request(connection).query(statement);
   winston.verbose(`=> `, result.recordset);
   return result.recordset;
 }
 
-export async function scalar(connection: Connection | Transaction, action: string, sql: string): Promise<any> {
-  const result = await query(connection, action, sql);
+export async function scalar(connection: sql.Connection | sql.Transaction, action: string, statement: string): Promise<any> {
+  const result = await query(connection, action, statement);
   return result.length > 0 ? result[0] : null;
 }
 
@@ -147,11 +146,11 @@ export function firstKeyValueOf(object: any): any {
 }
 
 export async function queryWithBoolResult(
-  connection: Connection | Transaction,
+  connection: sql.Connection | sql.Transaction,
   action: string,
-  sql: string,
+  statement: string,
 ): Promise<boolean> {
-  const result = await scalar(connection, action, sql);
+  const result = await scalar(connection, action, statement);
   return firstKeyValueOf(result) === 1;
 }
 
@@ -162,9 +161,9 @@ export async function executeGeneratedSql(generatedSql: string, databaseName: st
     async (db) => {
       winston.verbose(`[${databaseName}] executeGeneratedSql`);
       // eslint-disable-next-line no-restricted-syntax
-      for (const sql of sqlStatements) {
-        winston.debug('\n', highlightSql(sql));
-        if (sql != null || sql !== '') await new Request(db).query(sql);
+      for (const statement of sqlStatements) {
+        winston.debug('\n', highlightSql(statement));
+        if (statement != null || statement !== '') await new sql.Request(db).query(statement);
       }
     },
     true,
