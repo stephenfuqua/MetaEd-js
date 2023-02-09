590e32a1bc82e0b70d13aa58f1d6727a18fe6024
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2023-02-09 11:23:42 -0600

    [METAED-1307] Remove plugin generator (#114)
    * Drop plugin
    
    * npm dedupe
    
    * Remove generator-code

--- packages/generator-metaed-plugin/LICENSE.md
+++
@@ -1,1 +0,0 @@
-MetaEd is ©2023 Ed-Fi Alliance, LLC. Click [here](https://techdocs.ed-fi.org/x/vRSAAw) for license information.
--- packages/generator-metaed-plugin/generators/app/templates/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transformIgnorePatterns: ['<rootDir>.*(node_modules)(?!.*metaed-.*).*$'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['js', 'ts'],
-  modulePathIgnorePatterns: ['dist*'],
-};
