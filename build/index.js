e492004534159d6394b43c18328ef6e7dcc92dc8
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2023-03-27 16:12:31 -0500

    [METAED-1431] Restore npm install (#123)
    * Restore npm install
    
    * Restore loglevel setting

--- build/index.js
+++ build/index.js
@@ -1,3 +1,13 @@
+import { execSync } from 'child_process';
+
+try {
+  execSync('npm install --loglevel error');
+} catch (err) {
+  console.error('Exception occurred during npm install atom-metaed:');
+  console.error('--> stdout', err);
+  console.error('--> sdterr', err.stderr.toString());
+}
+
 /* eslint-disable no-console */
 console.info('Command arguments: ', process.argv);
 
