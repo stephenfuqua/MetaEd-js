f8f099dd8c128ed3b2167a7ce4ab2dd980d89747
Adam Hopkins <127156771+simpat-adam@users.noreply.github.com>
2024-03-27 17:39:21 -0500

    add square brackets to schema and table name on index creation (#219)

--- packages/metaed-plugin-edfi-ods-sqlserver/test/integration/artifact/v7_2/SimpleType-1410-Extension-Extension-CreateIndex-EdOrgIdsRelationship-AuthPerformance-Authoritative.sql
+++ packages/metaed-plugin-edfi-ods-sqlserver/test/integration/artifact/v7_2/SimpleType-1410-Extension-Extension-CreateIndex-EdOrgIdsRelationship-AuthPerformance-Authoritative.sql
@@ -1,20 +1,20 @@
 
 IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_ChartOfAccount_EducationOrganizationId' AND object_id = OBJECT_ID('extension.ChartOfAccount')) 
 BEGIN
-    CREATE INDEX IX_ChartOfAccount_EducationOrganizationId ON extension.ChartOfAccount(EducationOrganizationId) INCLUDE (Id)
+    CREATE INDEX IX_ChartOfAccount_EducationOrganizationId ON [extension].[ChartOfAccount](EducationOrganizationId) INCLUDE (Id)
 END;
 
 IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_LocalAccount_EducationOrganizationId' AND object_id = OBJECT_ID('extension.LocalAccount')) 
 BEGIN
-    CREATE INDEX IX_LocalAccount_EducationOrganizationId ON extension.LocalAccount(EducationOrganizationId) INCLUDE (Id)
+    CREATE INDEX IX_LocalAccount_EducationOrganizationId ON [extension].[LocalAccount](EducationOrganizationId) INCLUDE (Id)
 END;
 
 IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_LocalActual_EducationOrganizationId' AND object_id = OBJECT_ID('extension.LocalActual')) 
 BEGIN
-    CREATE INDEX IX_LocalActual_EducationOrganizationId ON extension.LocalActual(EducationOrganizationId) INCLUDE (Id)
+    CREATE INDEX IX_LocalActual_EducationOrganizationId ON [extension].[LocalActual](EducationOrganizationId) INCLUDE (Id)
 END;
 
 IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_LocalBudget_EducationOrganizationId' AND object_id = OBJECT_ID('extension.LocalBudget')) 
 BEGIN
-    CREATE INDEX IX_LocalBudget_EducationOrganizationId ON extension.LocalBudget(EducationOrganizationId) INCLUDE (Id)
+    CREATE INDEX IX_LocalBudget_EducationOrganizationId ON [extension].[LocalBudget](EducationOrganizationId) INCLUDE (Id)
 END;
