452793d8d0ce87c67cb6075d02e149b7faac7f92
Moises Siles <msiles@growthaccelerationpartners.com>
2024-08-07 10:36:27 -0600

    [DMS-299] - Push package Fix (#253)

--- eng/ApiSchema/build.ps1
+++ eng/ApiSchema/build.ps1
@@ -1,7 +1,7 @@
 [CmdLetBinding()]
 param (
     [string]
-    [ValidateSet("Clean", "Build", "BuildAndPublish", "PushPackage", "Unzip", "Package", "RunMetaEd", "MoveMetaEdSchema")]
+    [ValidateSet("DotNetClean", "Build", "BuildAndPublish", "PushPackage", "Unzip", "Package", "RunMetaEd", "MoveMetaEdSchema")]
     $Command = "Build",
 
     [String]
@@ -14,7 +14,7 @@ param (
     # This value needs to be replaced with the config file
     [string]
     $EdFiNuGetFeed = "https://pkgs.dev.azure.com/ed-fi-alliance/Ed-Fi-Alliance-OSS/_packaging/EdFi/nuget/v3/index.json",
-    
+
     # API key for accessing the feed above. Only required with with the Push
     # command.
     [string]
@@ -27,42 +27,36 @@ param (
     $PackageFile,
 
     [switch]
-    $DryRun
+    $DryRun,
+
+    [string]
+    $schemaPackagingConfigFile = "/home/runner/work/MetaEd-js/MetaEd-js/eng/ApiSchema/ApiSchemaPackaging-GitHub.json"
 )
 
 $solutionRoot = "$PSScriptRoot"
 $defaultSolution = "$solutionRoot/EdFi.DataStandard51.ApiSchema.sln"
 $applicationRoot = "$solutionRoot/"
 $projectName = "EdFi.DataStandard51.ApiSchema"
-Import-Module -Name "$PSScriptRoot/../../eng/build-helpers.psm1" -Force
 
 function Restore {
-    Invoke-Execute { dotnet restore $defaultSolution }
+    dotnet restore $defaultSolution
 }
 
 function DotNetClean {
-    Invoke-Execute { dotnet clean $defaultSolution -c $Configuration --nologo -v minimal }
-}
-
-function Invoke-Clean {
-    Invoke-Step { DotNetClean }
+    dotnet clean $defaultSolution -c $Configuration --nologo -v minimal
 }
 
 function Compile {
-    Invoke-Execute {
-        dotnet build $defaultSolution -c $Configuration -p:Version=$Version --nologo --no-restore
-    }
+    dotnet build $defaultSolution -c $Configuration -p:Version=$Version --nologo --no-restore
 }
 
 function PublishApi {
-    Invoke-Execute {
-        $project = "$applicationRoot"
-        $outputPath = "$project/publish"
-        dotnet publish $project -c $Configuration -o $outputPath --nologo
-    }
+    $project = $applicationRoot
+    $outputPath = "$project/publish"
+    dotnet publish $project -c $Configuration -o $outputPath --nologo
 }
 
-function Invoke-UnzipFile {    
+function Invoke-UnzipFile {
     Invoke-WebRequest "https://odsassets.blob.core.windows.net/public/project-tanager/5.1.0-xsd-and-metadata.zip" `
         -OutFile json-and-xsd-$Version.zip
     Expand-Archive json-and-xsd-$Version.zip
@@ -75,11 +69,11 @@ function PushPackage {
         [Parameter(Mandatory=$true)]
         [string]
         $EdFiNuGetFeed,
-        
+
         [Parameter(Mandatory=$true)]
         [string]
         $NuGetApiKey,
-    
+
         [string]
         $PackageFile,
 
@@ -87,48 +81,33 @@ function PushPackage {
         $DryRun
     )
 
-    Invoke-Execute {
-        if (-not $NuGetApiKey) {
-            throw "Cannot push a NuGet package without providing an API key in the `NuGetApiKey` argument."
-        }
-
-        if (-not $EdFiNuGetFeed) {
-            throw "Cannot push a NuGet package without providing a feed in the `EdFiNuGetFeed` argument."
-        }
-
-        if (-not $PackageFile) {
-            $PackageFile = "$PSScriptRoot/$projectName.$Version.nupkg"
-        }
-
-        if ($DryRun) {
-            Write-Info "Dry run enabled, not pushing package."
-        }
-        else {
-            Write-Info ("Pushing $PackageFile to $EdFiNuGetFeed")
-            dotnet nuget push $PackageFile --source $EdFiNuGetFeed --api-key $NuGetApiKey
-        }
+    if (-not $NuGetApiKey) {
+        throw "Cannot push a NuGet package without providing an API key in the `NuGetApiKey` argument."
     }
-}
 
-function Invoke-PushPackage {
-    Invoke-Step { 
-        PushPackage -EdFiNuGetFeed $EdFiNuGetFeed -NuGetApiKey $NuGetApiKey -PackageFile $PackageFile -DryRun:$DryRun
-    } -Arguments @{
-        EdFiNuGetFeed = $EdFiNuGetFeed;
-        NuGetApiKey = $NuGetApiKey;
-        PackageFile = $PackageFile;
-        DryRun = $DryRun;
+    if (-not $EdFiNuGetFeed) {
+        throw "Cannot push a NuGet package without providing a feed in the `EdFiNuGetFeed` argument."
+    }
+
+    if (-not $PackageFile) {
+        Write-Output "Not Package File specified."
+        $PackageFile = "$PSScriptRoot/$projectName.$Version.nupkg"
+        Write-Output "Package File: $PackageFile"
+    }
+
+    if ($DryRun) {
+        Write-Output "Dry run enabled, not pushing package."
+    }
+    else {
+        Write-Output "Pushing the NuGet Package $PackageFile to $EdFiNuGetFeed"
+        dotnet nuget push $PackageFile --source $EdFiNuGetFeed --api-key $NuGetApiKey
     }
 }
 
 function Invoke-Build {
-    Invoke-Step { DotNetClean }
-    Invoke-Step { Restore }
-    Invoke-Step { Compile }
-}
-
-function Invoke-BuildPackage {
-    Invoke-Step { BuildPackage }
+    DotNetClean
+    Restore
+    Compile
 }
 
 function RunNuGetPack {
@@ -139,73 +118,52 @@ function RunNuGetPack {
         [string]
         $PackageVersion
     )
-
     dotnet pack $ProjectPath -c release -p:PackageVersion=$Version --output $PSScriptRoot
 }
 
 function BuildPackage {
-    Write-Output "Building Package ($Version)"
     $mainPath = "$applicationRoot"
     $projectPath = "$mainPath/$projectName.csproj"
 
     RunNuGetPack -ProjectPath $projectPath -PackageVersion $Version
 }
 
-function Invoke-Publish {
-    Write-Output "Building Version ($Version)"
-    Invoke-Step { PublishApi }
-}
-
 function RunMetaEd {
-    Write-Output "Run MetadEd Project"
-    Invoke-Execute { npm install }
-    Invoke-Execute { npm run build }
+    # Run MetadEd Project
+    npm install
+    npm run build
     Set-Location -Path ./packages/metaed-console
 
-    Write-Output "Get Working Dir"
+    # Get Working Dir
     Get-Location
     Get-ChildItem
-    
+
     <#
     After building the project from the parent directory, we need to confirm it's functional using the following command,
-    which will use the provided config file. For more details, 
+    which will use the provided config file. For more details,
     please refer to the readme file located in ./packages/meteaed-console/src/README.md
-    #>     
-    Invoke-Execute { node ./dist/index.js -a `
-        -c /home/runner/work/MetaEd-js/MetaEd-js/eng/ApiSchema/ApiSchemaPackaging-GitHub.json }
+    #>
+    node ./dist/index.js -a -c $schemaPackagingConfigFile
 }
 
 function CopyMetaEdFiles {
-    Write-Output "Copy the MetaEd Files into the ApiSchema Folder"
-
-    Write-Output ("Copy the ApiSchema.json into the " + $solutionRoot)
+    # Copy the MetaEd Files into the ApiSchema Folder
     Copy-Item -Path ./MetaEdOutput/ApiSchema/ApiSchema/ApiSchema.json -Destination $solutionRoot
-    
-    Write-Output ("Copy the XSD content into the " + $solutionRoot + "/xsd")
+
     Copy-Item -Path ./MetaEdOutput/EdFi/XSD/* -Destination $solutionRoot/xsd/
 }
 
-function Invoke-RunMetaEd {
-    Invoke-Step { RunMetaEd }
-}
-
-function Invoke-CopyMetaEd {
-    Invoke-Step { CopyMetaEdFiles }
-}
-
-Invoke-Main {
-    switch ($Command) {
-        Clean { Invoke-Clean }
-        Build { Invoke-Build }
-        Unzip { Invoke-UnzipFile }
-        BuildAndPublish { 
-            Invoke-Build             
-            Invoke-Publish
-        }        
-        Package { Invoke-BuildPackage }
-        PushPackage { Invoke-PushPackage }
-        RunMetaEd { Invoke-RunMetaEd }
-        MoveMetaEdSchema { Invoke-CopyMetaEd }
-        default { throw "Command '$Command' is not recognized" }
+switch ($Command) {
+    DotNetClean { DotNetClean }
+    Build { Invoke-Build }
+    Unzip { Invoke-UnzipFile }
+    BuildAndPublish {
+        Invoke-Build
+        PublishApi
     }
+    Package { BuildPackage }
+    PushPackage { PushPackage $EdFiNuGetFeed }
+    RunMetaEd { RunMetaEd }
+    MoveMetaEdSchema { CopyMetaEdFiles }
+    default { throw "Command '$Command' is not recognized" }
 }
